864ac5b0e89e7411deb4966c5e32496df0298d27
Andri Óskarsson <account+github@andri.dk>
2025-06-07 09:40:16 +0200

    wip

--- text_value.go
+++ text_value.go
@@ -11,13 +11,12 @@ func (n Text) Type() NodeType {
 	return NodeTextValue
 }
 
-func (n Text) ArgumentType() ArgumentType {
-	return ArgumentText
-}
-
 func (n Text) WriteTo(w io.Writer) (int64, error) {
 	template.HTMLEscape(w, []byte(n))
 	return 0, nil
 }
 
 // TODO: I'd really like for Text to be work as an attribute too
+func (n Text) ArgumentType() ArgumentType {
+	return ArgumentText
+}
